63ebe35aaadc2f8b8bf3602b1f19d91c8761a871
Alex Koszuta <alexkoszuta43@gmail.com>
2025-05-04 01:35:29 -0400

    fix app name in browser tab and manifest

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,8 +3,6 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
 import './Navbar.css';
 import { useNavigate } from 'react-router-dom';
@@ -20,17 +18,14 @@ const Navbar = () => {
   return (
     <AppBar position="static" className="Navbar">
       <Toolbar>
-        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <MenuIcon />
-        </IconButton>
         <Typography
           variant="h6"
           component="div"
-          sx={{ flexGrow: 1 }}
+          sx={{ flexGrow: 1, cursor: 'pointer' }}
           className="MuiTypography-root"
           onClick={onTitleClick}
         >
-          Navbar
+          SpecView
         </Typography>
         <Button className="MuiButton-root">Login</Button>
       </Toolbar>
