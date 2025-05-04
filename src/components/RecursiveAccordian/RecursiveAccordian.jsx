24bd6d59d0362e7168460db2effa901d41d9c8d6
Alex Koszuta <alexkoszuta43@gmail.com>
2025-05-04 00:09:06 -0400

    allow default expanded on top level accordian

--- src/components/RecursiveAccordian/RecursiveAccordian.jsx
+++ src/components/RecursiveAccordian/RecursiveAccordian.jsx
@@ -4,44 +4,50 @@ import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import PropTypes from 'prop-types';
 
-const RecursiveAccordion = React.memo(({ data, BaseAccordianBody, AccordianSummaryBody }) => {
-  const children = Object.entries(data?.children || {}).map((c) => c[1]);
+const RecursiveAccordion = React.memo(
+  ({ data, BaseAccordianBody, AccordianSummaryBody, isTop }) => {
+    const children = Object.entries(data?.children || {}).map((c) => c[1]);
 
-  const [expanded, setExpanded] = useState(false);
+    const [expanded, setExpanded] = useState(!!isTop);
 
-  const handleChange = (_, isExpanded) => {
-    setExpanded(isExpanded);
-  };
+    const handleChange = (_, isExpanded) => {
+      setExpanded(isExpanded);
+    };
 
-  return (
-    <Accordion expanded={expanded} onChange={handleChange}>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <AccordianSummaryBody data={data} />
-      </AccordionSummary>
+    return (
+      <Accordion expanded={expanded} onChange={handleChange}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <AccordianSummaryBody data={data} />
+        </AccordionSummary>
 
-      {expanded && (
-        <AccordionDetails disableRipple>
-          {children?.length > 0 ? (
-            <div style={{ marginLeft: 16 }}>
-              {children.map((child, i) => {
-                return (
-                  <RecursiveAccordion
-                    key={i}
-                    data={child}
-                    BaseAccordianBody={BaseAccordianBody}
-                    AccordianSummaryBody={AccordianSummaryBody}
-                  />
-                );
-              })}
-            </div>
-          ) : (
-            <BaseAccordianBody data={data} />
-          )}
-        </AccordionDetails>
-      )}
-    </Accordion>
-  );
-});
+        {expanded && (
+          <AccordionDetails disableRipple>
+            {children?.length > 0 ? (
+              <div style={{ marginLeft: 16 }}>
+                {children.map((child, i) => {
+                  return (
+                    <RecursiveAccordion
+                      key={i}
+                      data={child}
+                      BaseAccordianBody={BaseAccordianBody}
+                      AccordianSummaryBody={AccordianSummaryBody}
+                    />
+                  );
+                })}
+              </div>
+            ) : (
+              <BaseAccordianBody data={data} />
+            )}
+          </AccordionDetails>
+        )}
+      </Accordion>
+    );
+  }
+);
+
+RecursiveAccordion.defaultProps = {
+  isTop: false,
+};
 
 export default RecursiveAccordion;
 
@@ -53,4 +59,5 @@ RecursiveAccordion.propTypes = {
   }).isRequired,
   BaseAccordianBody: PropTypes.elementType.isRequired,
   AccordianSummaryBody: PropTypes.elementType.isRequired,
+  isTop: PropTypes.bool,
 };
