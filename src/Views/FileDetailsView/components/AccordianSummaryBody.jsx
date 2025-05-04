09b861b9a07cecaa2d1f2ee911bddbcc99f87e04
Alex Koszuta <alexkoszuta43@gmail.com>
2025-05-04 13:02:04 -0400

    Replace box with status chip component

--- src/Views/FileDetailsView/components/AccordianSummaryBody.jsx
+++ src/Views/FileDetailsView/components/AccordianSummaryBody.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import StatusChip from '../../../components/StatusChip/StatusChip';
+import chipVariants from '../../../components/StatusChip/chipVariants';
 
 const AccordianSummaryBody = ({ data }) => {
   return (
@@ -12,10 +14,18 @@ const AccordianSummaryBody = ({ data }) => {
 
       <div className="flex accordianCountsWrapper">
         {typeof data.passed === 'number' && (
-          <div className="successBg successText summaryCountBox">{data.passed}</div>
+          <StatusChip
+            count={data.passed || '0'}
+            variant={chipVariants.success}
+            sx={{ padding: '0.2rem' }}
+          />
         )}
         {typeof data.failed === 'number' && (
-          <div className="errorBg ErrorText summaryCountBox">{data.failed}</div>
+          <StatusChip
+            count={data.failed || '0'}
+            variant={chipVariants.error}
+            sx={{ padding: '0.2rem' }}
+          />
         )}
       </div>
     </div>
@@ -33,7 +43,7 @@ AccordianSummaryBody.propTypes = {
         status: PropTypes.string.isRequired,
         failureMessages: PropTypes.arrayOf(PropTypes.string),
       })
-    ).isRequired,
+    ),
     failed: PropTypes.number,
     passed: PropTypes.number,
   }).isRequired,
